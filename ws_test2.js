71ffc1fe286938f54b3a6ecb3c3d1d1ef5a4fe18
leo153624nid <barjastroy@gmail.com>
2022-10-17 18:47:09 +0300

    Update ws_test2.js

--- ws_test2.js
+++ ws_test2.js
@@ -4,3 +4,13 @@
 // Необходимо написать функцию, которая вернет максимальное число слов в одном предложении.
 // Пример: Вход:
 const arr = ["Hello world", "This is a great solution", "How are you"]; // Ответ: 5.
+
+const maxWords = function (arr) {
+  const howManySpaces = (str) => {
+    let count = 0;
+    for (let i = 1; i < str.length - 1; i++) {
+      if (str[i] === " ") count++;
+    }
+    return count;
+  };
+};
